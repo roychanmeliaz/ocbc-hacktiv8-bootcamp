4ff8d396ae75c67b9fca75286a139fe345cbcd38
roychanmeliaz <roychan.meliaz@gmail.com>
2021-10-12 19:45:33 +0700

    added another file

---
+++ sesi_04/assignment1_joined.cs
@@ -0,0 +1,7 @@
+using System;
+
+class Assignment1 {
+    public static void Main() {
+        
+    }
+}
---
+++ sesi_04/assignment1_nomor7.cs
@@ -0,0 +1,15 @@
+using System;
+
+class assignment1_nomor7
+{
+    public static void Main() {
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine("             ABOUT ME             ");
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine();
+        Console.WriteLine("Nama:       Muhammad Roychan Meliaz");
+        Console.WriteLine("Kode:       FSDO001ONL021");
+        Console.WriteLine("Asal:       Kota Batam");
+        Console.WriteLine("Pendidikan: ITS, Teknik Komputer S1");
+    }
+}
