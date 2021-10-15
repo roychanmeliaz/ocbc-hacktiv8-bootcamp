f4a683da07695bc674b299541c13027f851bb309
roychanmeliaz <roychan.meliaz@gmail.com>
2021-10-15 11:44:06 +0700

    add session 9

---
+++ sesi_09/Kantor_WebAPI/Models/EmployeeItem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kantor_WebAPI.Models
+{
+    public class EmployeeItem
+    {
+        private Models.EmployeeContext context;
+
+        public int id {get; set;}
+        public string nama {get; set;}
+        public string jenisKelamin {get; set;}
+        public string alamat {get; set;}
+    }
+}
