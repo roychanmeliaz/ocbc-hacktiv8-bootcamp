628ba04d689b3e7c66b35193e524ffd2fde95b32
roychanmeliaz <roychan.meliaz@gmail.com>
2021-10-18 23:01:12 +0700

    session 12

---
+++ sesi_12/UnitTest_Mock/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitTest_Mock.Model;
+using UnitTest_Mock.Services;
+
+namespace UnitTest_Mock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        #region Property
+
+        private readonly IEmployeeService _employeeService;
+
+        #endregion
+
+        #region Constructor
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        #endregion
+
+        [HttpGet(nameof(GetEmployeeById))]
+        public async Task<string> GetEmployeeById(int EmpID)
+        {
+            var result = await _employeeService.GetEmployeebyId(EmpID);
+            return result;
+        }
+
+        [HttpGet(nameof(GetEmployeeDetails))]
+        public async Task<Employee> GetEmployeeDetails(int EmpId)
+        {
+            var result = await _employeeService.GetEmployeeDetails(EmpId);
+            return result;
+        }
+    }
+}
